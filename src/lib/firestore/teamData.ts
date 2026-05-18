e9b532206b149d546d501692e7ba057c9e00d838
jOlavi <juhasoftwaret@gmail.com>
2026-05-18 11:17:25 +0300

    feat: add Firestore team subcollection helpers

---
+++ src/lib/firestore/teamData.ts
@@ -0,0 +1,26 @@
+import { collection, doc, getDocs, setDoc, deleteDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+
+export async function getSubcollection<T>(
+  teamId: string,
+  sub: string
+): Promise<T[]> {
+  const snap = await getDocs(collection(db, 'teams', teamId, sub));
+  return snap.docs.map((d) => d.data() as T);
+}
+
+export function writeTeamDoc<T extends { id: string }>(
+  teamId: string,
+  sub: string,
+  data: T
+): void {
+  setDoc(doc(db, 'teams', teamId, sub, data.id), data).catch(console.error);
+}
+
+export function removeTeamDoc(
+  teamId: string,
+  sub: string,
+  id: string
+): void {
+  deleteDoc(doc(db, 'teams', teamId, sub, id)).catch(console.error);
+}
